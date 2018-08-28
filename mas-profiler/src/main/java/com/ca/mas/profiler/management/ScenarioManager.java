6b67f6cc2500b77c613fa029f2fd5078120676e3
Ruqiya <hkruqi@gmail.com>
2018-08-28 16:14:47 +0530

    Add time to execution

--- mas-profiler/src/main/java/com/ca/mas/profiler/management/ScenarioManager.java
+++ mas-profiler/src/main/java/com/ca/mas/profiler/management/ScenarioManager.java
@@ -1,57 +1,69 @@
-package com.ca.mas.profiler.management;
+    package com.ca.mas.profiler.management;
 
-import android.content.Context;
+    import android.content.Context;
+    import android.util.Log;
 
-import com.ca.mas.profiler.Scenario;
-import com.ca.mas.profiler.beans.ProfilerConfig;
-import com.ca.mas.profiler.beans.ScenarioInfo;
+    import com.ca.mas.profiler.Scenario;
+    import com.ca.mas.profiler.beans.ProfilerConfig;
+    import com.ca.mas.profiler.beans.ScenarioInfo;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+    import java.lang.reflect.Constructor;
+    import java.lang.reflect.InvocationTargetException;
+    import java.lang.reflect.Method;
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.Map;
 
-public class ScenarioManager {
-    private static final ScenarioManager instance = new ScenarioManager();
-    private List<ScenarioInfo> scenarios;
+    public class ScenarioManager {
+        private static final ScenarioManager instance = new ScenarioManager();
+        private List<ScenarioInfo> scenarios;
 
-    private ScenarioManager() {
+        private ScenarioManager() {
 
-    }
+        }
 
-    public static ScenarioManager getInstance() {
-        return instance;
-    }
+        public static ScenarioManager getInstance() {
+            return instance;
+        }
 
-    public void loadScenarios(Context context){
+        public void loadScenarios(Context context){
 
 
-        ConfigurationManager.getInstance().loadConfigurations(context);
-        scenarios = ConfigurationManager.getInstance().getAllScenarios();
-    }
+            ConfigurationManager.getInstance().loadConfigurations(context);
+            scenarios = ConfigurationManager.getInstance().getAllScenarios();
+        }
 
-    public void startScenarios(Context context)  {
-        ProfilerConfig profileConfig = ConfigurationManager.getInstance().getProfilerConfig();
-        int iterations=0;
-          if(profileConfig.getOperationType() == ProfilerConfig.OPERATION_TYPE.BENCHMARK){
-              iterations = profileConfig.getIterations();
-          }
-
-          for(int i=0; i<= iterations ; i++){
-
-              try {
-                  Class c = Class.forName(scenarios.get(i).getClazz());
-                  Method method = c.getMethod("evaluate", Context.class);
-                  //start time
-                  method.invoke(c.newInstance(), context);
-                  //end time
-              }catch (Exception e){
-
+        public void startScenarios(Context context)  {
+            ProfilerConfig profileConfig = ConfigurationManager.getInstance().getProfilerConfig();
+            int iterations=0;
+            double sum = 0.0;
+              if(profileConfig.getOperationType() == ProfilerConfig.OPERATION_TYPE.BENCHMARK){
+                  iterations = profileConfig.getIterations();
               }
-          }
+
+              for(int i=0; i<= iterations ; i++){
+
+                  try {
+                      Class c = Class.forName(scenarios.get(i).getClazz());
+                      Method method = c.getMethod("evaluate", Context.class);
+                      long start = System.nanoTime();
+                      method.invoke(c.newInstance(), context);
+                      long end = System.nanoTime();
+                      long duration = end - start;
+                      sum = sum+duration;
+                  }catch (Exception e){
+
+                  }
+              }
+            double avg;
+
+            if(iterations == 0) {
+                avg = sum;
+            } else {
+                avg = sum / iterations;
+            }
+            //TODO  Save result in file using analytics manager
+            Log.d("PROFILE", "Execution time for the scenario = "+ avg/1000000000.0 +" Seconds");
+        }
 
     }
-
-}
